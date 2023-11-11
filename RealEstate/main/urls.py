db511a3377a73b4060fd4194cb92b43008acd1f3
AlenaziHazal <hazalalenazi@gmail.com>
2023-11-11 20:06:20 +0300

    other pages are added

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -8,4 +8,7 @@ app_name = "main"
 urlpatterns = [
     path("", views.home_page_view , name="home_page_view"),
     path("properties/", views.properties_page_view, name="properties_page_view"),
+    path("our/service/", views.properties_page_view, name="service_page_view"),
+    path("contract/", views.properties_page_view, name="contract_page_view"),
+    path("about/us/", views.properties_page_view, name="about_page_view"),
 ]
