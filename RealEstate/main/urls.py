e8f6281891c9c0c08ee964c72a5e3a5f2f8605cc
AlenaziHazal <roomyham@MacBook-Pro.local>
2023-11-11 20:36:45 +0300

    everything is done

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -8,7 +8,7 @@ app_name = "main"
 urlpatterns = [
     path("", views.home_page_view , name="home_page_view"),
     path("properties/", views.properties_page_view, name="properties_page_view"),
-    path("our/service/", views.properties_page_view, name="service_page_view"),
-    path("contract/", views.properties_page_view, name="contract_page_view"),
-    path("about/us/", views.properties_page_view, name="about_page_view"),
+    path("our/service/", views.service_page_view, name="service_page_view"),
+    path("contract/", views.contract_page_view, name="contract_page_view"),
+    path("about/us/", views.about_page_view, name="about_page_view"),
 ]
