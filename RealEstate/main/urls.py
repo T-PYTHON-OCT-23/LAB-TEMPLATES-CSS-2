969c81ad2e48235c8033179529342f53f4e806b6
AlenaziHazal <hazalalenazi@gmail.com>
2023-11-11 19:59:22 +0300

    buge is solved

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -7,5 +7,5 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.home_page_view , name="home_page_view"),
-    path("/properties", views.properties_page_view, name="properties_page_view"),
+    path("properties/", views.properties_page_view, name="properties_page_view"),
 ]
