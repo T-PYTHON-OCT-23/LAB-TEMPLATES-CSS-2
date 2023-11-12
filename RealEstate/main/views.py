b8b4094e1d307cac54a6bac1a09a28b3a4efff16
oudy yousef <oudy.usef@gmail.com>
2023-11-12 06:10:09 +0300

    Done

---
+++ RealEstate/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+
+# Create your views here.
+
+def index_view(request:HttpRequest):
+    
+    return render(request,"main/home.html") 
+
