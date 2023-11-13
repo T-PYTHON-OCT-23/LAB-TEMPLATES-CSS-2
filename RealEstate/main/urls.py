8998a79dac621e2a3c951fe472580a7da346cd23
abdullah <abdullah.alanezi.23@gmail.com>
2023-11-13 21:47:29 +0300

    cookies lab

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -4,5 +4,10 @@ from .import views
 app_name ='main'
 
 urlpatterns =[
-    path('',views.index, name='home')
+    path('',views.index, name='home'),
+    path('about/',views.about,name='about'),
+    path('font/large/',views.large_font,name='font_large'),
+    path('font/small/',views.small_font,name='font_small')
+
+
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpRequest
 # Create your views here.
 
@@ -6,3 +6,19 @@ from django.http import HttpRequest
 def index(request:HttpRequest):
 
     return render(request,'main/index.html')
+
+def about(request:HttpRequest):
+
+    return render(request,'main/about.html')
+
+def large_font(request:HttpRequest):
+
+    response = redirect('main:home')
+    response.set_cookie('font','large')
+    return response
+
+def small_font(request:HttpRequest):
+
+    response= redirect('main:home')
+    response.set_cookie('font','small')
+    return response
