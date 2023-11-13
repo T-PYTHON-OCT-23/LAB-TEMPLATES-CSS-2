aae1a377dd513647b9589818257b1b40ea9c02e5
hadeel.i7 <hadeel.naji175@gmail.com>
2023-11-13 23:33:26 +0300

    cookie

--- YourHome/Home/urls.py
+++ YourHome/Home/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = "Home"
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("large/size/", views.large_size, name="large_size"),
+    path("small/size/", views.small_size, name="small_size"),
 ]
--- YourHome/Home/views.py
+++ YourHome/Home/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse, response
 
 # Create your views here.
 
 
 def home(request: HttpRequest):
     return render(request, 'Home/home.html')
+
+
+def about(request: HttpRequest):
+    response = render(request, 'Home/home.html')
+
+
+def large_size(request: HttpRequest):
+
+    response = redirect("main:about.viwes")
+    response.set_cookie("size", "large")
+    return response
+
+
+def small_size(request: HttpRequest):
+
+    response = redirect("main:about.viwes")
+    response.set_cookie("size", "small")
+    return response
