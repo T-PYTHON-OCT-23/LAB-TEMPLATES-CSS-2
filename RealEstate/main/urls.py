6144373ffb8a982d88c9a9705847e07a9169557a
Reef <reef.zayed2@gmail.com>
2023-11-13 18:50:00 +0300

    done

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -5,5 +5,8 @@ app_name = "main"
 
 
 urlpatterns = [
-    path("", views.home_views, name="home_views")
+    path("", views.home_views, name="home_views"),
+    path('about/', views.about_viwes, name="about_viwes"),
+    path('zoomout/', views.zoom_out, name="zoom_out"),
+    path('zoomin/', views.zoom_in, name="zoom_in")
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 def home_views(request:HttpRequest):
 
     return render(request , 'main/home.html')
+
+def about_viwes(request:HttpRequest):
+    return render(request , 'main/about.html')
+
+
+def zoom_in(requset: HttpRequest):
+
+    response = redirect("main:about_viwes")
+    response.set_cookie("mode", "zoom_in")
+
+    return response
+
+def zoom_out(requset: HttpRequest):
+
+    response = redirect("main:about_viwes")
+    response.set_cookie("mode", "zoom_out")
+
+    return response
+
