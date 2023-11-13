83622f5b1fd0ab880983f448b1b98a10489d40cf
alya <alyamem80@gmail.com>
2023-11-13 14:01:41 +0300

    done lap css2-2

--- RealEstate/main1/urls.py
+++ RealEstate/main1/urls.py
@@ -6,5 +6,8 @@ app_name = "main1"
 
 urlpatterns = [
     path("", views.home_view, name="home_view"),
+    path("another", views.another_view, name="another_view"),
+    path("mode/small/", views.small_mode_view, name="small_mode_view"),
+    path("mode/big/", views.big_mode_view, name="big_mode_view"),
     
 ]
--- RealEstate/main1/views.py
+++ RealEstate/main1/views.py
@@ -1,9 +1,8 @@
 
-from django.shortcuts import render, resolve_url
+from django.shortcuts import render, resolve_url , redirect
 from django.http import HttpRequest, HttpResponse
 from django.utils.crypto import get_random_string
 # Create your views here.
-
 
 
 
@@ -12,3 +11,34 @@ def home_view(request:HttpRequest):
     
     return render(request, 'main1/home.html')
 
+
+def another_view(request:HttpRequest):
+
+    
+    return render(request, 'main1/another.html')
+
+ #getting the cookies
+    print(request.COOKIES["mode"])
+
+    return render(request, "main1/another.html")
+
+
+def another_view(request: HttpRequest):
+
+    return render(request, "main1/another.html")
+
+
+def small_mode_view(requset: HttpRequest):
+
+    response = redirect("main1:another_view")
+    response.set_cookie("mode", "small")
+
+    return response
+
+
+def big_mode_view(requset: HttpRequest):
+
+    response = redirect("main1:another_view")
+    response.set_cookie("mode", "big")
+
+    return response
