c53ed3ddbe5c9f04f5e2cf4a378f881d60dd532f
Sufana_Aljawi <Sufanaaljawi@gmail.com>
2023-11-13 22:36:55 +0300

    Done-lab-Cookies

--- RealEstate/real_estate_app/urls.py
+++ RealEstate/real_estate_app/urls.py
@@ -6,5 +6,7 @@ app_name = "real_estate_app"
 
 urlpatterns = [
     path('', views.home_page, name="home_page"),
-
+    path('/about', views.about_page, name="about_page"),
+    path('font/large/', views.large_font_view, name="large_font_view"),
+    path('font/small/',views.small_font_view, name="small_font_view")
 ]
--- RealEstate/real_estate_app/views.py
+++ RealEstate/real_estate_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
 
@@ -6,3 +6,21 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 def home_page(request : HttpRequest):
     return render(request, "real_estate_app/home.html")
+
+def about_page(request : HttpRequest):
+    return render(request, "real_estate_app/about.html")
+
+def large_font_view(requset: HttpRequest):
+
+    response = redirect("real_estate_app:about_page")
+    response.set_cookie("font", "large")
+
+    return response
+
+
+def small_font_view(requset: HttpRequest):
+
+    response = redirect("real_estate_app:about_page")
+    response.set_cookie("font", "small")
+
+    return response
