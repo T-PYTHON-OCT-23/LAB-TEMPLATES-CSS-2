af8925855ac280f5f06ad6cbc32be0c4decbd8e7
danahalsalmi <danahalmenjaf@gmail.com>
2023-11-14 00:01:08 +0300

    done2

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -5,4 +5,9 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.home_view, name="home_view"),
+    path("about/" ,views.about_view,name="about_view"),
+    path("fontsize/large/", views.large_view, name="large_view"),
+    path("fontsize/small/",views.small_view , name="small_view"),
+
+
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render ,resolve_url
+from django.shortcuts import render ,redirect
 from django.http import HttpRequest, HttpResponse
 
 def home_view(request:HttpRequest):
+    print(request.COOKIES["fontsize"])
+       
     return render(request, 'main/home.html')
 
+
+def about_view(request: HttpRequest):
+
+    return render(request, "main/about.html")
+
+def large_view(requset: HttpRequest):
+
+    response = redirect("main:about_view")
+    response.set_cookie("fontsize", "large")
+
+    return response
+
+def small_view(requset: HttpRequest):
+
+    response = redirect("main:about_view")
+    response.set_cookie("fontsize", "small")
+
+    return response
 # Create your views here.
