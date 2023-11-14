ffc53ec0302ba06deee96eed85bdc667926fda7a
naif <iimnaif@gmail.com>
2023-11-14 03:02:24 +0300

    done

--- ProjectName/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest
-# Create your views here.
-
-def home_page_view(request:HttpRequest):
-
-    return render(request,'main/home.html')
