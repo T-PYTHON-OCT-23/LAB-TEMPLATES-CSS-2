f2578dd59e81886aa020147a1b66fb5debfa057b
ebtesam <ebtesam167@hotmail.com>
2023-11-14 07:22:42 +0300

    Done

--- RealEstate/RealEstate1/urls.py
+++ RealEstate/RealEstate1/urls.py
@@ -6,4 +6,8 @@ app_name = "RealEstate1"
 
 urlpatterns = [
     path("", views.homePage, name="homePage"),
+    path("about/", views.aboutPage, name="about"),
+    path("size/large/", views.large_size_view, name="large_size_view"),
+    path("size/small/", views.small_size_view, name="small_size_view"),
+     
 ]
--- RealEstate/RealEstate1/views.py
+++ RealEstate/RealEstate1/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpRequest, HttpResponse
 
 def homePage(request : HttpRequest):
     
     return render(request ,"RealEstate1/home.html")
+
+def aboutPage(request : HttpRequest):
+    
+    return render(request ,"RealEstate1/about.html")
+
+def large_size_view(request: HttpRequest):
+
+    response = redirect("RealEstate1:about")
+    response.set_cookie("size", "large")
+    
+    
+    return response 
+
+
+def small_size_view(request: HttpRequest):
+
+    response = redirect("RealEstate1:about")
+    response.set_cookie("size", "small")
+   
+
+    return response 
